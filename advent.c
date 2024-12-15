3aba7a482ca9785cfb7c3b5a08e4088564363052
Craig Spannring <ns-hub@spannring.org>
2024-12-14 22:56:12 -0700

    Better docs.

--- advent.c
+++ advent.c
@@ -367,4 +367,18 @@ char *game_name(char *filename)
 
 Colossal Cave Adventure is a text based adventure game.  Originally
 written in Fortran running on a DEC PDP-10.
+
+
+This version is based on BDS C Adventure.  From 
+[David Kinder's guide to Adventure downloads](https://rickadams.org/adventure/e_downloads.html)
+
+@verbatim
+ From the Jay R. Jaegar converted the original Adventure to BDS C, and
+ added separate values for treasures, survival and score. Available is
+ Bob Wither's DOS version, as a DOS executable and MS C source
+ code. The BDS code was converted to Unix by Jerry D. Pohl. This latter
+ version is available as Unixsource code, OS/2 source code, a DOS
+ executable and an OS/2 executable.
+@endverbatim
+
 */ 
